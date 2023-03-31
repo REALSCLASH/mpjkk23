638644f1b590a052887360c827f82f1b874657a7
sclash <samuelpellikka@gmail.com>
2023-03-31 10:39:34 +0300

    validation

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,7 +1,14 @@
+import {Box, Button, Container, TextField} from '@mui/material';
 import PropTypes from 'prop-types';
-import useForm from '../hooks/FormHooks';
-import {useUser} from '../hooks/ApiHooks';
 import React from 'react';
+import {useContext} from 'react';
+import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
+import {useNavigate} from 'react-router-dom';
+import {MediaContext} from '../contexts/MediaContext';
+import {useAuthentication, useUser} from '../hooks/apiHooks';
+import useForm from '../hooks/FormHooks';
+import { registerForm } from '../utils/errorMessages';
+import { registerValidators } from '../utils/validators';
 
 const RegisterForm = (props) => {
   const {postUser, getCheckUser} = useUser();
@@ -33,38 +40,56 @@ const RegisterForm = (props) => {
   );
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
+    <Container maxWidth="xs">
+      <ValidatorForm onSubmit={handleSubmit} noValidate>
+        <TextValidator
+          fullWidth
+          margin="dense"
           name="username"
-          placeholder="Username"
+          label="Username"
           onChange={handleInputChange}
           value={inputs.username}
           onBlur={handleUsername}
+          validators={registerValidators.username}
+          errorMessages={registerForm.username}
         />
-        <input
+        <TextValidator
+          fullWidth
+          margin="dense"
           name="password"
           type="password"
-          placeholder="Password"
+          label="Password"
           onChange={handleInputChange}
           value={inputs.password}
+          validators={registerValidators.password}
+          errorMessages={registerForm.password}
         />
-        <input
+        <TextValidator
+          fullWidth
+          margin="dense"
           name="email"
           type="email"
-          placeholder="Email"
+          label="Email"
           onChange={handleInputChange}
           value={inputs.email}
+          validators={registerValidators.email}
+          errorMessages={registerForm.email}
         />
-        <input
+        <TextValidator
+          fullWidth
+          margin="dense"
           name="full_name"
-          placeholder="Full name"
+          label="Full name"
           onChange={handleInputChange}
           value={inputs.full_name}
+          validators={registerValidators.full_name}
+          errorMessages={registerForm.full_name}
         />
-        <button type="submit">Register</button>
-      </form>
-    </>
+        <Button fullWidth sx={{mt: 1}} variant="contained" type="submit">
+          Register
+        </Button>
+      </ValidatorForm>
+    </Container>
   );
 };
 
